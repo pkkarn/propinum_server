82e99bee3b116ca3e2936518f6b68d78937753e1
prashant.k.ed <prashant.kumar@edvanta.com>
2023-06-18 00:19:39 +0530

    Added Users Custom API Key

--- controllers/proposalController.js
+++ controllers/proposalController.js
@@ -1,46 +1,65 @@
 const { generateProposalPrompt } = require('../prompts')
 const WorkSchema = require('../schema/WorkSchema')
-const propAI = require('../modules/streamGpt')
+const {streamCompletion, customStreamCompletion} = require('../modules/streamGpt')
 
 exports.generatePropsal = async (req,res,next) => {
-    if(req.user) {
-        /**
-         * Refactor
-         * 1. Check user credit
-         * 2. if its exhausted then return error to tell them buy
-         * 3. if its available then return response
-         */
+    try {
+        if(req.user) {
+            /**
+             * Refactor
+             * 1. Check user credit
+             * 2. if its exhausted then return error to tell them buy
+             * 3. if its available then return response
+             */
 
-        if(req.user.credits > 0) {
-                
-            try {
-                const { workId, gig_info } = req.body;
-
-                const work = await WorkSchema.findById(workId)
-                const prompt = generateProposalPrompt(gig_info, work.workBio);
-
-                console.log(prompt)
-                // Find workBio using workId and then pas into generateProposalPrompt and generate prompt
-                
-                req.user.credits -= 1;
-                
-              await propAI(prompt, (chunk) => {
-                    res.write(chunk);
-              });
-                // update user credits by one
-              await req.user.save();
-                res.end();
-            } catch (err) {
-                res.status(500).send({ message: 'Something went wrong, please try again later', err });
+            console.log(req.body)
+    
+            if(req.user.credits > 0) {
+                    const { workId, gig_info } = req.body;
+    
+                    const work = await WorkSchema.findById(workId)
+                    const prompt = generateProposalPrompt(gig_info, work.workBio);
+    
+                    console.log('User credits is being used')
+                    // Find workBio using workId and then pas into generateProposalPrompt and generate prompt
+                    
+                    req.user.credits -= 1;
+                    
+                  await streamCompletion(prompt, (chunk) => {
+                        res.write(chunk);
+                  });
+                    // update user credits by one
+                  await req.user.save();
+                    res.end();
+            } else {
+                if(req.body.openaiKey) {
+                    try{
+                        const { workId, gig_info } = req.body;
+        
+                        const work = await WorkSchema.findById(workId)
+                        const prompt = generateProposalPrompt(gig_info, work.workBio);
+        
+                        console.log('API is being used')
+                        // Find workBio using workId and then pas into generateProposalPrompt and generate prompt
+                      await customStreamCompletion(req.body.openaiKey, prompt, (chunk) => {
+                            res.write(chunk);
+                      });
+                        // update user credits by one
+                        res.end();
+                    } catch (err) {
+                        res.status(500).send({ message: 'API is invalid' });
+                    }
+                     
+                } else {
+                    res.status(400).send({ message: 'Your credits is exhausted, please buy more' });
+                }
+                // tell them to buy credits
             }
-
         } else {
-            // tell them to buy credits
-              res.status(400).send({ message: 'Your credits is exhausted, please buy more' });
+            res.status(404).send({ message: 'User not authenticated' });
         }
-
-
-    } else {
-        res.status(404).send({ message: 'User not authenticated' });
+    } catch (err) {
+        res.status(500).send({ message: 'Something went wrong, please try again later', err });
     }
+    
 }
--- modules/streamGpt.js
+++ modules/streamGpt.js
@@ -73,4 +73,75 @@ async function streamCompletion(prompt, onData) {
 	return processedChunks;
 }
 
-module.exports = streamCompletion;
+async function customStreamCompletion(openaiKey, prompt, onData) {
+	const headers = {
+		'Content-Type': 'application/json',
+		'Authorization': `Bearer ${openaiKey}`,
+	};
+
+	const data = {
+		model: 'text-davinci-003',
+		prompt,
+		temperature: 1,
+		max_tokens: 1500,
+		stream: true,
+	};
+
+	const res = await axios.post('https://api.openai.com/v1/completions', data, {
+		headers: headers,
+		responseType: 'stream',
+	});
+
+	// Remainder chunk - When json is incomplete
+	let remainderChunk = '';
+
+	let output = '';
+
+	const processedChunks = new Promise((resolve, reject) => {
+		res.data.on('data', (chunk) => {
+			const chunkStr = chunk.toString();
+			console.log(chunkStr);
+			if(chunkStr.trim() !== '[DONE]') {
+				// split data using data: as one chunk can contain multiple and also filter this those that starts with { or }
+				const item = chunkStr.split('data: ')
+					.map(i => i.trim())
+					.filter(i => (i.startsWith('{') || i.endsWith('}')));
+
+				item.forEach(i => {
+					// Filter out proper and complete data
+					if(i.startsWith('{') && i.endsWith('}')) {
+						const parsedChunk = JSON.parse(i);
+						output += parsedChunk.choices[0].text;
+						onData(parsedChunk.choices[0].text);
+					}
+					else {
+						// If its incomplete and starts with { then add remained that it needs to be added in next set
+						if(i.startsWith('{')) {
+							remainderChunk += i;
+						}
+						else {
+							// complete the current chunk with remained and mark remainder as an empty one
+							const finalChunk = remainderChunk + i;
+							remainderChunk = '';
+							const parsedFinalChunk = JSON.parse(finalChunk);
+							output += parsedFinalChunk.choices[0].text;
+							onData(parsedFinalChunk.choices[0].text);
+						}
+					}
+				});
+			}
+		});
+
+		res.data.on('end', () => {
+			resolve(output);
+		});
+
+		res.data.on('error', (err) => {
+			reject(err);
+		});
+	});
+
+	return processedChunks;
+}
+
+module.exports = {streamCompletion, customStreamCompletion};
